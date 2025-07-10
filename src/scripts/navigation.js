fc7f192f07fb121d47155dd7d2900d9a9dc5d929
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-07-10 17:37:28 +0200

    Added nav menu animations and js

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,8 +1,24 @@
 const menuButton = document.querySelector("#menu-button");
 const navMenu = document.querySelector("#nav-menu");
+const navList = document.querySelectorAll("#nav-menu li");
 
 menuButton.addEventListener("click", () => {
     navMenu.classList.replace("hidden", "flex");
-    console.log(navMenu);
+    navMenu.classList.add("animate-slide-in");
 });
 
+navList.forEach((liElement) => {
+    liElement.addEventListener("click", () => {    
+        navMenu.classList.replace("animate-slide-in", "animate-slide-out");
+    });
+});
+
+navMenu.addEventListener("animationend", (event) => {
+    if (event.animationName !== "slide-out") return;
+    navMenu.classList.replace("flex", "hidden");
+    navMenu.classList.remove("animate-slide-out");
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 640) navMenu.classList.remove("animate-slide-in", "animate-slide-out")
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-export default {
-  content: [
-    "./src/**/*.{html,js}",
-    "/*.html"
-  ],
-  theme: {
-    extend: {
-        animation: {
-        'slide-in': 'slide-in 0.5s ease-in-out',
-        'slide-out': 'slide-out 0.5s ease-in-out'
-        },
-        keyframes: {
-            'slide-in': {
-                '0%' : {transform: 'translate(-100%)'},
-                '100%': {transform: 'translate(0)'}
-            }
-        }
-    },
-  plugins: [],
-  }
-}
+// export default {
+//   content: [
+//     "./src/**/*.{html,js}",
+//     "/*.html"
+//   ],
+//   theme: {
+//     extend: {
+//         animation: {
+//         'slide-in': 'slide-in 0.5s ease-in-out',
+//         'slide-out': 'slide-out 0.5s ease-in-out'
+//         },
+//         keyframes: {
+//             'slide-in': {
+//                 '0%' : {transform: 'translate(-100%)'},
+//                 '100%': {transform: 'translate(0)'}
+//             }
+//         }
+//     },
+//   plugins: [],
+//   }
+// }

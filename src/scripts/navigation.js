8826217c0a0c4f641b78d7de6eb3910e405d90ee
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-07-13 18:20:04 +0200

    added final touches

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,24 +1,33 @@
 const menuButton = document.querySelector("#menu-button");
 const navMenu = document.querySelector("#nav-menu");
 const navList = document.querySelectorAll("#nav-menu li");
+const mainTitle = document.querySelector("#main-title");
 
 menuButton.addEventListener("click", () => {
-    navMenu.classList.replace("hidden", "flex");
-    navMenu.classList.add("animate-slide-in");
+  navMenu.classList.replace("hidden", "flex");
+  navMenu.classList.add("animate-slide-in");
 });
 
 navList.forEach((liElement) => {
-    liElement.addEventListener("click", () => {    
-        navMenu.classList.replace("animate-slide-in", "animate-slide-out");
-    });
+  liElement.addEventListener("click", () => {
+    navMenu.classList.replace("animate-slide-in", "animate-slide-out");
+  });
 });
 
 navMenu.addEventListener("animationend", (event) => {
-    if (event.animationName !== "slide-out") return;
-    navMenu.classList.replace("flex", "hidden");
-    navMenu.classList.remove("animate-slide-out");
+  if (event.animationName !== "slide-out") return;
+  navMenu.classList.replace("flex", "hidden");
+  navMenu.classList.remove("animate-slide-out");
 });
 
 window.addEventListener("resize", () => {
-    if (window.innerWidth >= 640) navMenu.classList.remove("animate-slide-in", "animate-slide-out")
-})
+  if (window.innerWidth >= 640)
+    navMenu.classList.remove("animate-slide-in", "animate-slide-out");
+});
+
+mainTitle.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
